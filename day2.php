19b8826d1f6ec743a5799af951c9a8306ae1254b
anilghale1 <ghalepasang5@gmail.com>
2023-12-31 22:01:49 +0545

    day2 concept of array learnt

---
+++ day2.php
@@ -0,0 +1,29 @@
+<?php 
+$name = null;
+
+## arrays
+
+$isOld = null;
+
+$name = "Anil Ghale";
+$companies = array("Netscape","leapFrog","Risekit");
+##associative_array
+$details = array("age" => 26, "location" => "kamalpokhari");
+
+## multi_dimensional_array
+$numbers = array(array(1,2,3),array(4,5,6),array(7,8,9));
+echo $name;
+echo "<br>";
+
+   echo "<pre>";
+var_dump($companies);
+ echo "<pre>";
+
+ echo "<pre>";
+    print_r($details);
+  echo "<pre>";
+
+  echo "<pre>";
+    print_r($numbers);
+  echo "<pre>";
+
