709482be35a4237d7bd5854fc3399124cb6dba09
anilghale1 <ghalepasang5@gmail.com>
2024-01-06 10:14:21 +0545

    practise some of get post header global

--- day3/index.php
+++ day3/index.php
@@ -1,43 +1,33 @@
 <?php
-session_start();
-if (!isset($_SESSION['target_number'])) {
-  $_SESSION['target_number'] =  rand(1, 25);
-}
-
-$target_number =  $_SESSION['target_number'];
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  $user_number = $_POST['number'];
-
-
-
-  if ($user_number == $target_number) {
-    echo "<h2>You are correct!</h2>";
-    session_destroy();
-  } else if ($user_number > $target_number) {
-    echo "<h2>Too High!</h2>";
-  } else {
-    echo "<h2>Too Low!</h2>";
-  }
-}
-
+   if($_SERVER['REQUEST_METHOD'] == "GET" )
+       {
+        echo "you are so stupid!";
+       }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Guessing game</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
 </head>
-
 <body>
-  <h1>Guess the number i am thinking</h1>
-  <form method="POST">
-    <input autofocus type="number" name="number" id="">
-    <button type="submit">Check</button>
-  </form>
+        <form  method="GET">  
+              <input name="userNAme" />
+              <button type="submit">Send</button>  
+             
+        </form>
+ 
+         <!-- <form method="POST">    
+            <input name="userNAme">
+            <input name="passWord">
+            <button>Send</button> 
+            //no transparency at browser high level abstraction   embed at body //if change arrive use POST
+                default method get embed at browser //if no change use GET  if no security concern use GET
+      // by default it is submit
+        </form> 
+      
+            </form>    -->
 </body>
-
 </html>
---
+++ day3/search.php
@@ -0,0 +1,18 @@
+<?php
+
+  
+   echo "<pre>";
+   print_r($_GET);
+   echo "</pre>";
+
+
+   echo "you are so handsome" . $_GET['search'];
+   echo "<br>";
+   
+   //  header("Location: index.html");   //redirect
+   ## Post ==> $_POST;
+   ## GET ==> $_GET;
+
+   ?>
+
+   <a href="/">GO back to home page</a>
